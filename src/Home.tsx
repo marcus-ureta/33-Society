e8f4297c62b2633b8221b10e80f28f18372c6efe
Arjan <subtlerotation64@gmail.com>
2026-09-02 17:51:51 +0800

    feat: create Navbar draft mockup

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="w-full h-dvh bg-tristesse-0"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@base-ui/react/button";
+import "./index.css";
+
+import { Logo } from "@/components/logos";
+
+function Navbar() {
+  return (
+    <>
+      <div className="fixed w-9/10 max-w-270 left-1/2 -translate-x-1/2 h-16 top-12 bg-selago-0 rounded-full flex flex-row items-center px-6">
+        <Logo variant="horizontal" className="h-12 w-auto text-schiava-blue" />
+        <div className="w-full flex flex-row gap-10 px-8 *:font-aileron *:text-schiava-blue">
+          <p>One Time Event Pass</p>
+          <p>About</p>
+          <p>Members</p>
+          <p>Event Details</p>
+        </div>
+        <Button className={"text-selago-0 bg-schiava-blue px-4"}>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
