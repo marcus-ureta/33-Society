c6455542189b64013ed4d4b067bfc1ae550ac6f8
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 18:04:47 +0800

    login page implemented

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -2,7 +2,7 @@
 import { buttonVariants } from './components/ui/button.tsx'
 import { cn } from './lib/utils'
 
-import { ArrowRight } from "flowbite-react-icons/outline";
+import { ArrowRight, ArrowLeft } from "flowbite-react-icons/outline";
 import { Logo } from '@/components/logos/Logo.tsx'
 
 function loginSignUpPage() {
@@ -53,7 +53,7 @@ function passWordPage() {
             </div>
 
             <div className='flex flex-col sm:flex-row items-center justify-center w-full h-[80%] gap-x-2.5 gap-y-2.5'>
-                <input placeholder='Password' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic pl-[8px]" />
+                <input name='password'placeholder='Password' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic pl-[8px]" />
 
                 <ArrowRight className="w-6 h-6 text-davys-grey-0 hover:text-selago-0 border-2 rounded-full mt-2 transition-all duration-200 hover:cursor-pointer" />
             </div>
@@ -62,10 +62,50 @@ function passWordPage() {
     )
 }
 
+function loginPage() {
+    return(
+        <>
+            <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
+                
+                <div className="flex flex-col gap-y-4 items-center">
+                    <Logo variant="primary" className="size-16 text-selago-100" />
+                </div>
+
+                <div className='flex flex-col items-center justify-center w-full h-[80%] gap-y-16'>
+
+                    <div className='flex flex-col gap-y-9 items-center'>
+                        <input name='email' placeholder='Email' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic pl-[8px]" />
+
+                        <input name='password' placeholder='Password' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic pl-[8px]" />
+                    </div>
+
+                    <div className='flex flex-col gap-y-6 items-center'>
+                        <button className={cn(buttonVariants({ variant: "default", size: "lg" }),
+                            "bg-schiava-blue text-white shadow-[0_0_13px_1px_var(--color-tristesse-50)] border-2 border-schiava-blue-dark py-4 px-12 rounded-[64px] hover:bg-schiava-blue-dark hover:shadow-[0_0_13px_1px_var(--color-tristesse-50)] hover:scale-102 hover:cursor-pointer transition-all duration-300 font-['Aileron'] font-semibold text-[1rem]",
+                        )}>
+                            LOGIN
+                        </button>
+
+                        <h2 className="text-selago-100 underline transition-colors duration-200 cursor-pointer font-['Aileron'] text-[1rem] hover:text-selago-0">
+                            Forget Password
+                        </h2>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-col items-center justify-center w-full h-[10%] gap-y-2.5'>
+                    <ArrowLeft className="w-6 h-6 text-davys-grey-0 hover:text-selago-0 border-2 rounded-full mt-2 transition-all duration-200 hover:cursor-pointer"/>
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
 function Portal() {
     return (
         <>
-            {passWordPage()}
+            {loginPage()}
         </>
     )
 }
