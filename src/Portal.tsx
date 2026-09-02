747e8fa3cb383d96e9fe9afa70f12f63961bcf17
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 16:18:29 +0800

    creation of Password Page

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -36,10 +36,25 @@ function loginSignUpPage() {
     )
 }
 
+function passWordPage() {
+    return(
+        <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
+            <div className="flex flex-col gap-y-4 items-center">
+                <img className="w-16 h-16" src="/svg/33.svg" alt="Logo" />
+
+                <div className="flex gap-x-3 items-center w-[320px]">
+                    <div className="w-full h-[3px] bg-davys-grey-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
+                    <div className="w-full h-[3px] bg-davys-grey-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function Portal() {
     return (
         <>
-            {loginSignUpPage()}
+            {passWordPage()}
         </>
     )
 }
