4ecc985b0ae0b0a3c38c3e3788cc0cc85983a00a
Arjan <subtlerotation64@gmail.com>
2026-09-02 16:17:19 +0800

    feat: create reusable svg logo component

---
+++ src/components/logos/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./PrimaryLogo";
+export * from "./Logomark";
+export * from "./Wordmark";
+export * from "./HorizontalLockup";
+export * from "./StackedLockup";
+export * from "./Logo";
