3fe3f9230897294c01fdb82eee63bdf934496326
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 15:36:53 +0800

    progress bar styling

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -1,20 +1,16 @@
 
-function Portal(){
-    return(
-        <>
-            <div className='w-screen h-dvh bg-tristesse-0 flex justify-center pt-8'>
+function Portal() {
+    return (
+        <div className="w-screen h-dvh bg-tristesse-0 flex justify-center pt-8">
+            <div className="flex flex-col gap-y-6 items-center">
+                <div className="w-16 h-16 bg-red-800 rounded-full" />
 
-                {/* LOGO AND PROGRESS BAR */}
-                <div className='flex flex-col gap-y-6 items-center'>
-                    <div className='w-16 h-16 bg-red-800 rounded-full'/>
-
-                    <div className='flex flex-row gap-x-3 items-center w-[320px] h-[3px] overflow-clip'>
-                        <div className='w-full h-full bg-selago-0 rounded-[10px]'/>
-                        <div className='w-full h-full bg-selago-0 rounded-[10px]'/>
-                    </div>
+                <div className="flex gap-x-3 items-center w-[320px]">
+                    <div className="w-full h-[3px] bg-white rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
+                    <div className="w-full h-[3px] bg-white rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
