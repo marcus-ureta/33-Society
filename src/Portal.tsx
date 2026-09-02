acd4cb95853c28d0eb41f58aba54d8939abca564
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 17:49:26 +0800

    imported proper logo and hover effectts

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -3,13 +3,14 @@ import { buttonVariants } from './components/ui/button.tsx'
 import { cn } from './lib/utils'
 
 import { ArrowRight } from "flowbite-react-icons/outline";
+import { Logo } from '@/components/logos/Logo.tsx'
 
 function loginSignUpPage() {
 
     return(
         <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
             <div className="flex flex-col gap-y-4 items-center">
-                <img className="w-16 h-16" src="/svg/33.svg" alt="Logo" />
+                <Logo variant="primary" className="size-16 text-selago-100" />
 
                 <div className="flex gap-x-3 items-center w-[320px]">
                     <div className="w-full h-[3px] bg-selago-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
@@ -43,7 +44,7 @@ function passWordPage() {
         <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
             
             <div className="flex flex-col gap-y-4 items-center">
-                <img className="w-16 h-16" src="/svg/33.svg" alt="Logo" />
+                <Logo variant="primary" className="size-16 text-selago-100" />
 
                 <div className="flex gap-x-3 items-center w-[320px]">
                     <div className="w-full h-[3px] bg-davys-grey-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
@@ -52,9 +53,9 @@ function passWordPage() {
             </div>
 
             <div className='flex flex-col sm:flex-row items-center justify-center w-full h-[80%] gap-x-2.5 gap-y-2.5'>
-                <input placeholder='Password' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic" />
+                <input placeholder='Password' className="border-b-[1px] border-davys-grey-0 text-[2rem] text-davys-grey-0 font-['instrument-serif'] italic pl-[8px]" />
 
-                <ArrowRight className="w-6 h-6 text-davys-grey-0 hover:text-selago-0 border-2 rounded-full mt-2 transition-all duration-200" />
+                <ArrowRight className="w-6 h-6 text-davys-grey-0 hover:text-selago-0 border-2 rounded-full mt-2 transition-all duration-200 hover:cursor-pointer" />
             </div>
             
         </div>
