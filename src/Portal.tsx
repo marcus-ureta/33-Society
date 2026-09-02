9c25abc2d653848f3b94e2636eebf98f27f7bb89
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 15:40:55 +0800

    Logo Implemented + Login & Sign-Up Button

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -1,14 +1,19 @@
 
 function Portal() {
     return (
-        <div className="w-screen h-dvh bg-tristesse-0 flex justify-center pt-8">
-            <div className="flex flex-col gap-y-6 items-center">
-                <div className="w-16 h-16 bg-red-800 rounded-full" />
+        <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
+            <div className="flex flex-col gap-y-4 items-center">
+                <img className="w-16 h-16" src="/svg/33.svg" alt="Logo" />
 
                 <div className="flex gap-x-3 items-center w-[320px]">
                     <div className="w-full h-[3px] bg-white rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
                     <div className="w-full h-[3px] bg-white rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
                 </div>
+            </div>
+
+            <div className='flex flex-col gap-y-4 items-center justify-center w-full h-full'>
+                <button>Sign-Up</button>
+                <button>Login</button>
             </div>
         </div>
     )
