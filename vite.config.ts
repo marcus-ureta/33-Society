65cd1f1ebc9d878f99bce1f8803082af4a64cfb9
Marcus Ureta <silvergreat0@gmail.com>
2026-09-02 15:53:20 +0800

    Button + updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    tsconfigPaths: true,
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
