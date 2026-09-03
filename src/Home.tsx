e3ca1b3fa4b4c0a1b165b47e31d369f199e9777b
Arjan <subtlerotation64@gmail.com>
2026-09-03 11:42:32 +0800

    feat: add crown page cut design to homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,6 +5,12 @@ import Navbar from "./Navbar";
 import { Button } from "@base-ui/react/button";
 
 function App() {
+  const SVG_WIDTH = 837.5;
+  const SVG_HEIGHT = 181.5;
+
+  // This calculates the aspect ratio (e.g., 200 / 1200 = 0.1666)
+  const HEIGHT_MULTIPLIER = SVG_HEIGHT / SVG_WIDTH;
+
   return (
     <>
       <Navbar />
@@ -30,7 +36,32 @@ function App() {
               <p className="leading-0 px-6 text-selago-0">Book a Meeting</p>
             </div>
           </div>
-          <div className="flex justify-center p-16"></div>
+            <div className="flex justify-center h-96 w-full"></div>
+          </div>
+
+          <div className="relative w-full @container h-[750px] -mt-36">
+            <div
+              className="absolute inset-0 bg-[url(/sample.png)] bg-no-repeat bg-size-[100%_100%]"
+              className="absolute inset-0 bg-schiava-blue bg-no-repeat bg-size-[100%_100%]"
+              style={{
+                maskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black)`,
+                WebkitMaskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black)`,
+
+                maskRepeat: "no-repeat, no-repeat",
+                WebkitMaskRepeat: "no-repeat, no-repeat",
+
+                // Layer 1: Scale width to 100% and let height adapt naturally.
+                // Layer 2: Fill 100% of the width and height of the container.
+                maskSize: "100% auto, 100% 100%",
+                WebkitMaskSize: "100% auto, 100% 100%",
+
+                // Layer 1: Pin to top left.
+                // Layer 2: Push it down exactly to where the scaled SVG height ends using Container Query Width (cqw).
+                maskPosition: `0 0, 0 calc((100cqw * ${HEIGHT_MULTIPLIER}) - 2px)`,
+                WebkitMaskPosition: `0 0, 0 calc((100cqw * ${HEIGHT_MULTIPLIER}) - 2px)`,
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
