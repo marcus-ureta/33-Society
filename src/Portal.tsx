62cfb2309b9e866598629a6d9f784f7b46dd0f2c
Marcus Ureta <silvergreat0@gmail.com>
2026-09-03 17:11:18 +0800

    page transitioning implementation

--- src/Portal.tsx
+++ src/Portal.tsx
@@ -18,10 +18,12 @@ import crown from '/svg/CROWN_CROPPED.svg';
 const Page = {
     password: 'Password',
     portal: 'Portal',
+
     signup: 'Signup',
     questionnaire: 'Questionnaire',
     accountSetup: 'AccountSetup',
     congratulations: 'Congratulations',
+
     login: 'Login',
 } as const;
 
@@ -50,7 +52,7 @@ function PasswordPage({setPage} : {setPage : React.Dispatch<React.SetStateAction
 }
 
 
-function LoginPage() {
+function LoginPage({setPage} : {setPage : React.Dispatch<React.SetStateAction<Page>>}) {
     return(
         <>
             <div className="w-screen h-dvh bg-tristesse-0 flex flex-col pt-8">
@@ -81,7 +83,7 @@ function LoginPage() {
 
                 </div>
 
-                <div className='flex flex-col items-center justify-center w-full h-[10%] gap-y-2.5'>
+                <div className='flex flex-col items-center justify-center w-full h-[10%] gap-y-2.5' onClick={() => setPage(Page.portal)}>
                     <ArrowLeft className="w-6 h-6 hover:text-schiava-blue-light text-selago-0 border-2 rounded-full mt-2 transition-all duration-200 hover:cursor-pointer"/>
                 </div>
                 
@@ -98,7 +100,7 @@ function LoginPage() {
 // |____________________________________| \\
 
 
-function SignUpPage() {
+function SignUpPage({setPage} : {setPage : React.Dispatch<React.SetStateAction<Page>>}) {
 
     return(
         <>
@@ -182,7 +184,7 @@ function SignUpPage() {
                             </div>
 
                             <div className='flex flex-col items-center justify-center mt-[4%]'>
-                                <Button type="submit" variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
+                                <Button onClick={() => setPage(Page.questionnaire)} variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
                                     "bg-schiava-blue text-white border-2 border-schiava-blue-dark py-5 px-12 rounded-[64px] hover:bg-schiava-blue-dark hover:scale-102 hover:cursor-pointer transition-all duration-300 font-['Aileron'] font-semibold text-[1.575rem]",
                                 )}>
                                     Submit
@@ -194,7 +196,7 @@ function SignUpPage() {
                 </div>
 
 
-                <div className='flex flex-col items-center justify-center w-full h-[10%] gap-y-2.5'>
+                <div className='flex flex-col items-center justify-center w-full h-[10%] gap-y-2.5' onClick={() => setPage(Page.portal)}>
                     <ArrowLeft className="w-6 h-6 hover:text-schiava-blue-light text-selago-0 border-2 rounded-full mt-2 transition-all duration-200 hover:cursor-pointer"/>
                 </div>
             </div> 
@@ -202,9 +204,9 @@ function SignUpPage() {
     )
 }
 
-function QuestionnairePage(){
+function QuestionnairePage({setPage} : {setPage : React.Dispatch<React.SetStateAction<Page>>}){
 
-    const [questionNo, setQuestionNo] = useState<number>(0);
+    const [questionNo, setQuestionNo] = useState<number>(1);
 
     const questions: string[] = [
         'What unique value or insight can you contribute to the community?',
@@ -216,11 +218,13 @@ function QuestionnairePage(){
     const handleNextQuestion = () => {
         console.log('firing function!');
 
-        if(questionNo <= 2) setQuestionNo(prev => (prev <= questions.length ? prev + 1 : prev));
+        if(questionNo <= 3) setQuestionNo(prev => (prev <= questions.length ? prev + 1 : prev))
+        else if(questionNo > 1) setPage(Page.accountSetup);
     }
 
     const handlePrevQuestion = () => {
-        if(questionNo > 0) setQuestionNo(prev => (prev <= questions.length ? prev - 1 : prev));
+        if(questionNo > 1) setQuestionNo(prev => (prev <= questions.length ? prev - 1 : prev))
+        else setPage(Page.signup);
     }
 
     return(
@@ -232,7 +236,7 @@ function QuestionnairePage(){
                     <div className="flex flex-row items-center w-[75%] max-w-[820px] gap-x-4 gap-y-7">
                         <div className="w-[50%] h-[3px] bg-selago-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]"/>
                         <div className="w-[50%] h-[3px] bg-selago-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]"/>
-                        {[...Array(6)].map((_, index) => (
+                        {[...Array(4)].map((_, index) => (
                             <div key={index} className="w-[50%] h-[3px] bg-davys-grey-0 rounded-full shadow-[0_0_16px_1px_var(--color-schiava-blue-dark)]" />
                         ))}
                     </div>
@@ -240,7 +244,7 @@ function QuestionnairePage(){
 
                 <div className='flex flex-col items-center justify-center w-full h-[80%] gap-y-4 sm:gap-y-12 max-w-none prose'>
                     <h1 className="font-['Cochin'] font-bold text-selago-100 text-3xl sm:text-4xl mx-12 text-center max-w-[1068px]">
-                        {questions[questionNo]}
+                        {questions[questionNo - 1]}
                     </h1>
 
                     <div className='flex flex-col sm:flex-row items-center justify-center gap-x-2.5 gap-y-2.5 w-full'>
@@ -262,7 +266,7 @@ function QuestionnairePage(){
     )
 }
 
-function AccountSetup(){
+function AccountSetup({setPage} : {setPage : React.Dispatch<React.SetStateAction<Page>>}){
     return (
         <div className="relative w-screen h-dvh bg-tristesse-0 flex flex-col pt-8 overflow-hidden">
 
@@ -283,7 +287,7 @@ function AccountSetup(){
                     </FieldGroup>
 
                     <div className="flex justify-center mt-[4%]">
-                        <Button type="submit" variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),
+                        <Button onClick={() => setPage(Page.congratulations)} variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),
                             "bg-schiava-blue text-white border-2 border-schiava-blue-dark py-5 px-12 rounded-[64px] hover:bg-schiava-blue-dark hover:scale-102 transition-all duration-300 font-['Aileron'] font-semibold text-[1.575rem]"
                         )}
                     >
@@ -356,13 +360,13 @@ function Portal() {
                     <div className='flex flex-col items-center justify-center w-full h-[80%]'>
 
                         <div className='grid grid-rows-2 gap-y-4'>
-                            <Button type="submit" variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
+                            <Button onClick={() => setPage(Page.signup)} variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
                                             "bg-schiava-blue text-white border-2 border-schiava-blue-dark py-5 px-12 rounded-[64px] hover:bg-schiava-blue-dark hover:scale-102 hover:cursor-pointer transition-all duration-300 font-['Aileron'] font-semibold text-[1.575rem]",
                                         )}>
                                 SIGN UP
                             </Button>
 
-                            <Button type="submit" variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
+                            <Button onClick={() => setPage(Page.login)} variant="outline" className={cn(buttonVariants({ variant: "default", size: "lg" }),
                                             "bg-schiava-blue text-white border-2 border-schiava-blue-dark py-5 px-12 rounded-[64px] hover:bg-schiava-blue-dark hover:scale-102 hover:cursor-pointer transition-all duration-300 font-['Aileron'] font-semibold text-[1.575rem]",
                                         )}>
                                 LOGIN
@@ -371,6 +375,26 @@ function Portal() {
 
                     </div>
                 </>
+            )}
+
+            {page === Page.login && (
+                <LoginPage setPage={setPage}/>
+            )}
+
+            {page === Page.signup && (
+                <SignUpPage setPage={setPage}/>
+            )}
+
+            {page === Page.questionnaire && (
+                <QuestionnairePage setPage={setPage}/>
+            )}
+
+            {page === Page.accountSetup && (
+                <AccountSetup setPage={setPage}/>
+            )}
+
+            {page === Page.congratulations && (
+                <CongratulationsPage/>
             )}
 
         </div>
