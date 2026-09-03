b44186f7d22fd9d80938e378f16004367a1d5947
Arjan <subtlerotation64@gmail.com>
2026-09-03 16:04:11 +0800

    feat: finish crown masks & create draft S33 about section

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "./components/logos";
 import "./index.css";
 
 import Navbar from "./Navbar";
@@ -5,11 +6,17 @@ import Navbar from "./Navbar";
 import { Button } from "@base-ui/react/button";
 
 function App() {
-  const SVG_WIDTH = 837.5;
-  const SVG_HEIGHT = 181.5;
+  const CROWN_TOP_WIDTH = 837.5;
+  const CROWN_TOP_HEIGHT = 181.5;
 
   // This calculates the aspect ratio (e.g., 200 / 1200 = 0.1666)
-  const HEIGHT_MULTIPLIER = SVG_HEIGHT / SVG_WIDTH;
+  const CROWN_TOP_HEIGHT_MULTIPLIER = CROWN_TOP_HEIGHT / CROWN_TOP_WIDTH;
+
+  const CROWN_BOTTOM_WIDTH = 1000;
+  const CROWN_BOTTOM_HEIGHT = 281.2;
+
+  const CROWN_BOTTOM_HEIGHT_MULTIPLIER =
+    CROWN_BOTTOM_HEIGHT / CROWN_BOTTOM_WIDTH;
 
   return (
     <>
@@ -40,28 +47,51 @@ function App() {
             <div className="flex justify-center h-72 w-full"></div>
           </div>
 
-          <div className="relative w-full @container h-[750px] -mt-36">
+          {/* -mt-36  pushes crown page cut upwards*/}
+          <div className="relative w-full @container h-fit p-8 flex flex-col items-center justify-center">
             <div
-              className="absolute inset-0 bg-[url(/sample.png)] bg-no-repeat bg-size-[100%_100%]"
               className="absolute inset-0 bg-schiava-blue bg-no-repeat bg-size-[100%_100%]"
               style={{
-                maskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black)`,
-                WebkitMaskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black)`,
+                maskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black), url('/svg/BOTTOM_CROWN.svg')`,
+                WebkitMaskImage: `url('/svg/CROWN_CROPPED.svg'), linear-gradient(black, black), url('/svg/BOTTOM_CROWN.svg')`,
 
-                maskRepeat: "no-repeat, no-repeat",
-                WebkitMaskRepeat: "no-repeat, no-repeat",
+                maskRepeat: "no-repeat, no-repeat, no-repeat",
+                WebkitMaskRepeat: "no-repeat, no-repeat, no-repeat",
 
-                // Layer 1: Scale width to 100% and let height adapt naturally.
-                // Layer 2: Fill 100% of the width and height of the container.
-                maskSize: "100% auto, 100% 100%",
-                WebkitMaskSize: "100% auto, 100% 100%",
+                // Layer 1: Scale width to 100% and explicitly match the top crown aspect ratio height.
+                // Layer 2: Fill 100% height minus top crown and bottom crown (with 4px overlap for seams).
+                // Layer 3: Scale width to 100% and explicitly match the bottom crown aspect ratio height.
+                maskSize: `100% auto, 100% calc(100% - (100cqw * ${CROWN_TOP_HEIGHT_MULTIPLIER}) - (100cqw * ${CROWN_BOTTOM_HEIGHT_MULTIPLIER}) + 4px), 100% auto`,
+                WebkitMaskSize: `100% auto, 100% calc(100% - (100cqw * ${CROWN_TOP_HEIGHT_MULTIPLIER}) - (100cqw * ${CROWN_BOTTOM_HEIGHT_MULTIPLIER}) + 4px), 100% auto`,
 
                 // Layer 1: Pin to top left.
-                // Layer 2: Push it down exactly to where the scaled SVG height ends using Container Query Width (cqw).
-                maskPosition: `0 0, 0 calc((100cqw * ${HEIGHT_MULTIPLIER}) - 2px)`,
-                WebkitMaskPosition: `0 0, 0 calc((100cqw * ${HEIGHT_MULTIPLIER}) - 2px)`,
+                // Layer 2: Push down to where the top crown ends (minus 2px to prevent a seam).
+                // Layer 3: Pin to bottom.
+                maskPosition: `0 0, 0 calc((100cqw * ${CROWN_TOP_HEIGHT_MULTIPLIER}) - 2px), 0 bottom`,
+                WebkitMaskPosition: `0 0, 0 calc((100cqw * ${CROWN_TOP_HEIGHT_MULTIPLIER}) - 2px), 0 bottom`,
               }}
             />
+
+            <div
+              className="w-full h-full z-1 relative flex flex-col items-center prose prose-lg"
+              style={{
+                paddingTop: `calc(((100cqw * ${CROWN_TOP_HEIGHT_MULTIPLIER}) - 2px) + 32px)`,
+                paddingBottom: `calc(100cqw * ${CROWN_BOTTOM_HEIGHT_MULTIPLIER})`,
+              }}
+            >
+              <h1 className="font-cochin text-selago-50 mb-0 text-3xl text-center">
+                ABOUT
+              </h1>
+              <Logo variant="mark" className="text-selago-50 size-36"></Logo>
+              <p className="text-center text-selago-0 font-aileron">
+                Oat cake cheesecake caramels shortbread toffee wafer. Tiramisu
+                sweet bonbon tart lemon drops. Muffin cake sugar plum chocolate
+                bar cotton candy sweet jujubes danish. Croissant icing chupa
+                chups jelly beans fruitcake.
+              </p>
+            </div>
+          </div>
+
           </div>
         </div>
       </div>
