8bd95c8e0c2dc637ebf2e22a79ef34f7af11051d
Arjan <subtlerotation64@gmail.com>
2026-09-03 16:00:31 +0800

    fix: restart hero section to focus on mobile first

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,11 +13,12 @@ function App() {
 
   return (
     <>
-      <Navbar />
-      <div className="w-full h-fit">
-        <div className="bg-tristesse-0 w-full h-dvh grid grid-rows-[65%_35%] grid-cols-1 justify-center">
-          <div className="prose prose-lg max-w-none flex flex-col items-center self-end">
-            <h1 className="font-cochin font-bold text-selago-100 text-8xl w-full max-w-[20ch] text-center mb-2">
+      {/* <Navbar /> */}
+      <div className="w-full h-fit flex justify-center bg-tristesse-0 min-w-80 *:wrap-break-word ">
+        <div className="max-w-480 flex flex-col w-full min-w-80">
+          <div className=" w-full h-fit flex flex-col items-center pt-32 p-8">
+            <div className="prose prose-lg max-w-none w-full flex flex-col items-center">
+              <h1 className="font-cochin font-bold text-selago-100 text-7xl w-full max-w-[20ch] text-center mb-2">
               Sweet topping shortbread jelly beans
             </h1>
             <p className="font-aileron text-selago-0 mb-0 text-center">
@@ -25,10 +26,10 @@ function App() {
               sweet danish sesame snaps.
             </p>
             {/* Hero CTA */}
-            <div className="mt-12 border-2 border-schiava-blue p-2 rounded-full flex flex-row">
+              <div className="mt-12 border-2 border-schiava-blue p-4 rounded-[42px] flex flex-col items-center gap-4">
               <Button
                 className={
-                  "text-selago-0 bg-schiava-blue font-aileron rounded-full text-base px-8 py-2"
+                    "text-selago-0 bg-schiava-blue font-aileron rounded-full text-base px-10 py-4"
                 }
               >
                 Purchase a Ticket
@@ -36,7 +37,7 @@ function App() {
               <p className="leading-0 px-6 text-selago-0">Book a Meeting</p>
             </div>
           </div>
-            <div className="flex justify-center h-96 w-full"></div>
+            <div className="flex justify-center h-72 w-full"></div>
           </div>
 
           <div className="relative w-full @container h-[750px] -mt-36">
