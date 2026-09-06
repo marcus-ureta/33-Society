f8c7f1829eed31f4fb69b3c29271d3e4f629e69a
Marcus Ureta <silvergreat0@gmail.com>
2026-09-06 16:58:44 +0800

    Error Message Imported to Account Registration

--- src/portal/accountSetup/AccountSetup.tsx
+++ src/portal/accountSetup/AccountSetup.tsx
@@ -22,13 +22,21 @@ import { useState } from 'react';
 
 
 function EmailVerification({setPage, email, formAnswers} : {setPage : React.Dispatch<React.SetStateAction<Page>>, email : string, formAnswers : FormAnswers}){
+    const [isRequest, setIsRequest] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     async function handleVerification(){
         try{
+            setErrorMessage('');
+            setIsRequest(true);
             await checkVerification(formAnswers);
             setPage(Page.congratulations);
         }catch(e){
             console.log(e);
+
+            setErrorMessage('Account not verified yet!');
+        }finally {
+            setIsRequest(false);
         }
     }
 
@@ -38,12 +46,20 @@ function EmailVerification({setPage, email, formAnswers} : {setPage : React.Disp
                 <h1 className="font-['Cochin'] text-3xl text-selago-0 font-bold text-center">We've sent a verification link to: {email}</h1>
                 <h2 className="font-['Aileron'] text-xl text-selago-0 mb-[16px] text-center">Please verify your email, then click the button below</h2>
 
-                <Button onClick={handleVerification} variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),
-                        "button-styling"
+                <Button onClick={handleVerification} variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),"button-styling")}>
+                    {isRequest ? (
+                        <>
+                            <span className="spinner"/>
+                            Registering Account...
+                        </>
+                    ) : (
+                        "Register Account"
                     )}
-                >
-                    Check Verification
                 </Button>
+
+                {errorMessage && (
+                    <p className="text-red-400 text-sm font-['Aileron'] text-center mt-[16px]">{errorMessage}</p>
+                )}
             </div>
         </>
     )
@@ -52,6 +68,8 @@ function EmailVerification({setPage, email, formAnswers} : {setPage : React.Disp
 function AccountSetup({setPage, email, formAnswers} : {setPage : React.Dispatch<React.SetStateAction<Page>>, email : string, formAnswers : FormAnswers}){
 
     const [goVerificationPage, setVerificationPage] = useState<boolean>(false);
+    const [isRequest, setIsRequest] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     async function handleSignup(event : React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -66,11 +84,24 @@ function AccountSetup({setPage, email, formAnswers} : {setPage : React.Dispatch<
         const password = formData.get('password')?.toString();
 
         try {
+            setIsRequest(true);
             const user = await createAccount(registeredEmail!, password!);
             setVerificationPage(true);
             console.log("Account created:", user.uid);
-        } catch (error) {
+        } catch (error : any) {
             console.error("Signup failed:", error);
+
+            if (error.code === "auth/email-already-in-use") {
+                setErrorMessage("Email already in use");
+            } else if (error.code === "auth/weak-password") {
+                setErrorMessage("Password is too weak");
+            } else if (error.code === "auth/invalid-email") {
+                setErrorMessage("Invalid email address");
+            } else {
+                setErrorMessage("Something went wrong. Please try again.");
+            }
+        } finally {
+            setIsRequest(false);
         }
     }
 
@@ -89,22 +120,31 @@ function AccountSetup({setPage, email, formAnswers} : {setPage : React.Dispatch<
 
                         <FieldGroup className="flex flex-col gap-y-6 my-[5%]">
                             <Field>
-                                <Input name='email_address' id="email_address" type="email" placeholder="33society@gmail.com" required className="bg-tristesse-0 border-davys-grey-100 text-selago-0 !text-[17px] h-fit py-[6px] px-2" defaultValue={email}/>
+                                <Input name='email_address' id="email_address" type="email" placeholder="33society@gmail.com" required className="bg-tristesse-0 border-davys-grey-100 text-selago-0 !text-[17px] h-fit py-[6px] px-2" defaultValue={email} onChange={() => setErrorMessage('')}/>
                             </Field>
 
                             <Field>
-                                <Input name='password' id="password" type="password" placeholder="Setup Your Password" required className="bg-tristesse-0 border-davys-grey-100 text-selago-0 !text-[17px] h-fit py-[6px] px-2"/>
+                                <Input name='password' id="password" type="password" placeholder="Setup Your Password" required className="bg-tristesse-0 border-davys-grey-100 text-selago-0 !text-[17px] h-fit py-[6px] px-2" onChange={() => setErrorMessage('')}/>
                             </Field>
                         </FieldGroup>
 
                         <div className="flex justify-center mt-[4%]">
-                            <Button type='submit' variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),
-                                "button-styling"
-                            )}
-                        >
-                                Register Account
+                            <Button disabled={isRequest} type='submit' variant="outline" className={cn(buttonVariants({variant: "default", size: "lg",}),
+                                "button-styling")}>
+                                {isRequest ? (
+                                    <>
+                                        <span className="spinner"/>
+                                        Registering Account...
+                                    </>
+                                ) : (
+                                    "Register Account"
+                                )}
                             </Button>
                         </div>
+
+                        {errorMessage && (
+                            <p className="text-red-400 text-sm font-['Aileron'] text-center mt-[16px]">{errorMessage}</p>
+                        )}
                     </form>
 
                         <Logo variant="stacked" className="hidden sm:block size-64 lg:size-96 xl:size-[28rem] text-selago-100 shrink-0"/>
