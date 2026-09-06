5fe0f8c6d96aa2ab668092a0e8843e37cd0ae109
Arjan <subtlerotation64@gmail.com>
2026-09-06 12:00:21 +0800

    feat: add md and partial lg breakpoints for navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -23,21 +23,47 @@ function Navbar() {
     <>
       <div className="fixed w-9/10 max-w-270 left-1/2 -translate-x-1/2 h-16 top-12 bg-selago-0 rounded-full grid grid-cols-[auto_1fr_auto] grid-rows-1 items-center px-6 z-50">
         <Logo variant="horizontal" className="h-6 w-auto text-schiava-blue" />
-        <div className="w-full flex flex-row gap-10 px-8 *:font-aileron *:text-schiava-blue *:text-base">
+
+        {/* LARGE TABLET AND UP ONLY LINKS */}
+        <div className="hidden w-full lg:flex flex-row gap-10 px-8 *:font-aileron *:text-schiava-blue *:text-base">
           {/* NAVBAR LINKS 
               TODO: react router links (scrollable ids) and single source array
           */}
-          {/* <p>One Time Event Pass</p>
+          <p>One Time Event Pass</p>
           <p>About</p>
           <p>Members</p>
-          <p>Event Details</p> */}
+          <p>Event Details</p>
         </div>
 
-        {/* MOBILE NAVBAR MENU */}
+        <div className="hidden md:flex flex-row gap-4 md:w-full lg:w-fit justify-end">
+          <Button
+            variant="outline"
+            className={
+              "text-schiava-blue px-5 py-2 font-aileron text-sm rounded-full h-full border-schiava-blue"
+            }
+          >
+            Book a Meeting
+          </Button>
+
+          <Button
+            variant="default"
+            className={
+              "text-selago-0 bg-schiava-blue px-5 py-2 font-aileron text-sm rounded-full h-full -ml-2"
+            }
+          >
+            Purchase a Ticket
+          </Button>
+        </div>
+
+        {/* SMALL TABLET AND BELOW NAVBAR MENU */}
         <Sheet>
           <SheetTrigger
             render={
-              <Button variant="ghost" size="icon-lg" className={"lg:hidden"}>
+              <Button
+                variant="ghost"
+                size="icon-lg"
+                className={"lg:hidden justify-self-end"}
+              >
                 <Bars className="size-6 text-schiava-blue" />
               </Button>
             }
@@ -64,11 +90,19 @@ function Navbar() {
               ></SheetClose>
             </div>
 
-            <div className="flex flex-col items-center w-full h-fit gap-8 prose prose-lg">
-              <p className="font-aileron text-selago-0 mb-0 mt-0">Link</p>
-              <p className="font-aileron text-selago-0 mb-0 mt-0">Link</p>
-              <p className="font-aileron text-selago-0 mb-0 mt-0">Link</p>
-              <p className="font-aileron text-selago-0 mb-0 mt-0">Link</p>
+            <div className="flex flex-col items-center w-full h-fit gap-8 prose prose-lg max-w-none">
+              <p className="font-aileron text-selago-0 mb-0 mt-0 text-center">
+                Link
+              </p>
+              <p className="font-aileron text-selago-0 mb-0 mt-0 text-center">
+                Link
+              </p>
+              <p className="font-aileron text-selago-0 mb-0 mt-0 text-center">
+                Link
+              </p>
+              <p className="font-aileron text-selago-0 mb-0 mt-0 text-center">
+                Link
+              </p>
             </div>
 
             <SheetFooter>
@@ -81,14 +115,6 @@ function Navbar() {
             </SheetFooter>
           </SheetContent>
         </Sheet>
-
-        {/* <Button
-          className={
-            "text-selago-0 bg-schiava-blue px-5 py-2 font-aileron text-sm rounded-full"
-          }
-        >
-          Book a Meeting
-        </Button> */}
       </div>
     </>
   );
