109dee781454e8ada951e97c20e3a2362a59dd01
Marcus Ureta <silvergreat0@gmail.com>
2026-09-12 21:49:00 +0800

    feat: updated generatePassword to gather input

--- scripts/generatePassword.ts
+++ scripts/generatePassword.ts
@@ -1,6 +1,16 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-function myFunction(name: string) {
-    console.log(`Hello, ${name}!`);
+async function passwordGenerator() {
+    const rl = readline.createInterface({ input, output });
+
+    try {
+        const input = await rl.question('Input Password: ');
+
+        console.log(`Successfully added ${input} to database`);
+    } finally {
+        rl.close();
+    }
 }
 
-myFunction("World");
+passwordGenerator();
