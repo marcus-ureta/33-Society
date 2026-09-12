e4207194a312fc3576364a1ea5b58b491afe54aa
Marcus Ureta <silvergreat0@gmail.com>
2026-09-12 23:13:51 +0800

    feat: Password Generator Implemented

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ import {onCall, CallableRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 import { initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
 
 initializeApp();
 
@@ -50,3 +50,24 @@ export const authenticatePassword = onCall( async (request : CallableRequest<aut
         }
     }
 })
+
+export const registerPassword = onCall(async (request) => {
+    const { password } = request.data;
+
+    if (!password || typeof password !== "string") {
+        throw new Error("Invalid password");
+    }
+
+    const db = getFirestore();
+
+    const doc = await db.collection("registeredPasswords").add({
+        password,
+        isActive: true,
+        createdAt: FieldValue.serverTimestamp(),
+    });
+
+    return {
+        success: true,
+        id: doc.id,
+    };
+});
--- scripts/generatePassword.ts
+++ scripts/generatePassword.ts
@@ -1,13 +1,39 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDi1suVEaddQO9uQLhfO_L0Ome7KYxwAro",
+    authDomain: "society-33.firebaseapp.com",
+    projectId: "society-33",
+    storageBucket: "society-33.firebasestorage.app",
+    messagingSenderId: "337627806420",
+    appId: "1:337627806420:web:8902f635a5599e06f59941",
+    measurementId: "G-N19HYCF249"
+};
+
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app);
+
+
 async function passwordGenerator() {
     const rl = readline.createInterface({ input, output });
 
     try {
         const input = await rl.question('Input Password: ');
 
-        console.log(`Successfully added ${input} to database`);
+        const registerPassword = httpsCallable(functions, "registerPassword");
+
+        const result = await registerPassword({
+            password: input,
+        });
+
+        console.log(`Successfully added ${input} to database as document!`);
+        console.log(result.data);
+    } catch (error) {
+        console.error("Failed to add password:", error);
     } finally {
         rl.close();
     }
